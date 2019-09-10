1e53d2c7b2cbfa979beef62857a34dddc8a1fccf
AlexB02 <alexanderdb8@gmail.com>
2019-09-10 20:51:33 +0100

    Fixed render_template issue

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,9 @@
-from flask import Flask
+import flask
 
-app = Flask("__main__")
+app = flask.Flask("__main__")
 
 @app.route("/")
 def index():
-    return flask.rendertemplate("index.html",token="React+Flask")
+    return flask.render_template("index.html",token="React+Flask")
 
 app.run(debug=True)
