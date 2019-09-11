f0dc99ca16b19e6836f7b1055f978b926802ed16
Alexander Bainbridge <abain001@altwoodschool.co.uk>
2019-09-11 12:11:26 +0100

    Username & Password login

--- backend/main.py
+++ backend/main.py
@@ -3,13 +3,27 @@ from flask import request, url_for
 
 app = flask.Flask("__main__")
 
+def authenticate(username,password):
+
+    if username == "abain001" and password == "password":
+
+        return True
+
 @app.route("/", methods=["GET","POST"])
 def index():
     loginstatus = ""
     if request.method == "POST":
-        name = request.form["name"]
+        username = request.form["name"]
         password = request.form["password"]
-        loginstatus = "Welcome " + name
+
+        if authenticate(username,password):
+
+            loginstatus = "Authorised"
+
+        else:
+
+            loginstatus = "Unauthorised"
+
     return flask.render_template("index.html",loginstatus=loginstatus)
 
 @app.route("/about")
