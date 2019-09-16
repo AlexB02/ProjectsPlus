d6404c62ccd76e714edac85d3da8eb134addc5a2
Alexander Bainbridge <abain001@altwoodschool.co.uk>
2019-09-16 14:43:32 +0100

    Added about page as test, removed login.html added about route for testing

--- backend/main.py
+++ backend/main.py
@@ -5,34 +5,21 @@ import sys
 
 app = flask.Flask("__main__")
 
-@app.route("/", methods=["GET","POST"])
+@app.route("/")
 def index():
 
-    sp.createDatabase()
-
+    #sp.createDatabase()
+    """
     if request.method == "POST":
 
         username = request.form["username"]
         email = request.form["email"]
-        return redirect(url_for("login", username=username, email=email))
+        """
+    return flask.render_template("index.html")
 
-    return flask.render_template("index.html",loginurl=url_for("login"))
+@app.route("/about")
+def about():
 
-@app.route("/login")
-def login(username="usernamedefault",email="emaildefault"):
-
-    try:
-        username = request.args["username"]
-        email = request.args["email"]
-
-        if username == "" or email == "":
-            username="usernamedefault"
-            email="emaildefault"
-
-    except:
-        username=username
-        email=email
-
-    return flask.render_template("login.html",username=username,email=email)
+    return flask.render_template("about.html")
 
 app.run(debug=True)
