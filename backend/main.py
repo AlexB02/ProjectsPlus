8494d01149e0a90aa1ecad7a87bf46e15bc1b235
AlexB02 <alexanderdb8@gmail.com>
2019-09-17 22:34:13 +0100

    main.py comments

--- backend/main.py
+++ backend/main.py
@@ -14,28 +14,40 @@ def index():
     loginmessage = ""
     signupmessage = ""
 
+    # If a form is submitted
     if flask.request.method == "POST":
 
+        # If the login form was submitted
         if request.form["identifier"] == "login":
 
+            # Attempt to get email and password
             try:
+
                 email = request.form["email"]
                 password = request.form["password"]
 
+                # Check if login is valid
                 if email == "alex@email.com" and password == "a":
                     loginmessage = "login successful"
+
                     return flask.render_template("index.html",loginmessage=loginmessage)
+
+                # Can't obtain login details
                 else:
                     loginmessage = "email or password incorrect"
                     return flask.render_template("index.html",loginmessage=loginmessage)
+
+            # Error in obtaining login details
             except:
                 loginmessage = "login failed, please try again"
                 return flask.render_template("index.html",loginmessage=loginmessage)
+
+        # If the sign up form was submitted
         else:
 
             signupmessage = "Tried to sign up"
             return flask.render_template("index.html",loginmessage=loginmessage,signupmessage=signupmessage)
-
+            
     return flask.render_template("index.html",loginmessage=loginmessage)
 
 app.run(debug=True)
