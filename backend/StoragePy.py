fd41f140cf7cc4ba88fae3b265a69f4878e2e79b
Alexander Bainbridge <abain001@altwoodschool.co.uk>
2019-09-19 10:08:36 +0100

    Added table creation

--- backend/StoragePy.py
+++ backend/StoragePy.py
@@ -4,10 +4,20 @@ def connect():
 
     conn = sql.connect('main.db')
     c = conn.cursor()
-    create()
+    create(c)
 
-def create():
+def create(c):
     try:
         c.execute("""CREATE TABLE members (id, firstname, lastname, email, passwordhash)""")
+    except:
+        pass
+
+    try:
+        c.execute("""CREATE TABLE skillslist (id, title)""")
+    except:
+        pass
+
+    try:
+        c.execute("""CREATE TABLE efficiencylist (memberid, skillid, date, time, efficiency)""")
     except:
         pass
