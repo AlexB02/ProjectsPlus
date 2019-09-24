989c05a2491d6c779c43927769f07bb97a1caaf8
Alexander Bainbridge <abain001@altwoodschool.co.uk>
2019-09-24 15:11:36 +0100

    Saltpoint variation

--- backend/main.py
+++ backend/main.py
@@ -10,15 +10,22 @@ app = flask.Flask("__main__")
 app.secret_key = secrets.token_urlsafe(24)
 
 def verifypassword(password,storedpassword):
-    storedkey = str(storedpassword[32:]).encode("utf-8")
-    storedsalt = str(storedpassword[:32]).encode("utf-8")
+    for saltpoint in range(1,186):
+        #saltpoint = 32
+        storedkey = str(storedpassword)[saltpoint:187].encode("utf-8")
+        storedsalt = str(storedpassword)[2:saltpoint].encode("utf-8")
 
-    verifykey = hashlib.pbkdf2_hmac('sha256', password.encode(encoding='utf-8'), storedsalt, 400000)
+        verifykey = str(hashlib.pbkdf2_hmac('sha256', password.encode(encoding='utf-8'), storedsalt, 400000))[saltpoint:187].encode("utf-8")
 
-    if storedkey == verifykey:
-        return True
-    else:
-        return False
+        print("trying new saltpoint: "+str(saltpoint))
+
+        print("Stored key: "+str(storedkey))
+        print("Calculated key: "+str(verifykey))
+
+        if storedkey == verifykey:
+            return True
+        #else:
+    return False
 
 @app.route("/",methods=["POST","GET"])
 def index():
@@ -29,23 +36,25 @@ def index():
 
 @app.route("/login", methods=["POST","GET"])
 def login():
+    try:
+        sp.connect()
 
-    sp.connect()
+        email = request.form["email"]
+        password = request.form["password"]
 
-    email = request.form["email"]
-    password = request.form["password"]
+        # Check if login is valid
+        if sp.recordExists(email) and verifypassword(password,sp.getStoredPassword(email)):
 
-    # Check if login is valid
-    if sp.recordExists(email) and verifypassword(password,sp.getStoredPassword(email)):
+            loginmessage = "login successful"
 
-        loginmessage = "login successful"
+        # Can't obtain login details
+        else:
 
-    # Can't obtain login details
-    else:
+            loginmessage="email or password incorrect"
 
-        loginmessage="email or password incorrect"
-
-    return jsonify({ "result" : "success", "loginmessage" : loginmessage})
+        return jsonify({ "result" : "success", "loginmessage" : loginmessage})
+    except:
+        return flask.redirect("/")
 
 
 
