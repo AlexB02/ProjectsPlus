f6433af5bb6d54662f3e808d231b691aea421339
Alexander Bainbridge <abain001@altwoodschool.co.uk>
2019-10-10 11:05:54 +0100

    Main.py comments

--- backend/main.py
+++ backend/main.py
@@ -24,61 +24,83 @@ def load_user(id):
 
 # Set the secret key of the site to a randomly generated string of letters, numbers and characters
 app.secret_key = secrets.token_urlsafe(32)
+# Set the time for a valid login (test value)
 app.config['PERMANENT_SESSION_LIFETIME'] =  timedelta(seconds=5)
 
+# Function to test the given password against the password stored for the entered email
 def verifypassword(password,storedpassword):
+    # Get the stored password
     storedpassword = storedpassword[0][0]
 
+    # Obtain the salt from the stored password (first 32 bits)
     salt = storedpassword[:32]
+    # Hash the given password with the same salt and hashing format as the stored password
     passwordhash = hashlib.pbkdf2_hmac('sha256', password.encode(encoding='utf-8'), salt, 400000)
+    # Put the hash of the given password in the same format as the stored password to give a comparable value
     passwordhash = salt + passwordhash
 
+    # Check if the given password is the same as the stored password
     if str(passwordhash) == str(storedpassword):
         return True
     else:
         return False
 
-def redirectToDashboard():
-
-    return flask.redirect("/dashboard")
-
-
+# Homepage route, takes post and get mathods for login and signup value transfer
 @app.route("/",methods=["POST","GET"])
 def index():
-    session["authenticated"] = False
+    # Set the authenticated cookie to false as they are not logged in
+    session["authenticated"] = "False"
+    # Set both the login and signup messages to blank to clear the form messages & make the site look clean
     loginmessage = ""
     signupmessage = ""
+    # Show the user the index (home) page
     return flask.render_template("index.html")
 
+# Login route, takes post and get methods for login value transfer
 @app.route("/login", methods=["POST","GET"])
 def login():
     try:
+        # Set up connection to the database (Subroutine from StoragePy)
         sp.connect()
-        session["authenticated"] = False
+        # Set the authenticated cookie to False
+        session["authenticated"] = "False"
 
+        # Get the email value from the form
         email = request.form["email"]
+        # Get the password value from the form
         password = request.form["password"]
 
         # Check if login is valid
         if sp.recordExists(email) and verifypassword(password,sp.getStoredPassword(email)):
 
+            # Set the login message to Valid Login
             loginmessage = "Valid login"
+
+            # Attempt to log in the user with flask-login to ensure that they will be successfully created as a user upon reaching the dashboard
             try:
                 if fl.login_user(sp.userObj(sp.getIDbyEmail(email)),remember=True,force=True):
 
                     fl.login_user(sp.userObj(sp.getIDbyEmail(email)),remember=True,force=True)
 
+                    # Change the login message to login successful to show that they will be created as a user successfully upon reaching the dashboard so they may continue
                     loginmessage = "login successful"
 
+                    # Ensure the user is authenticated correctly
                     if fl.current_user.is_authenticated:
 
+                        # Set the authentication cookie to True
                         session["authenticated"] = "True"
+                        # Set the email cookie to the email address of login
                         session["email"] = email
+                        # Set the session to permanent
                         session.permanent = True
                         return jsonify({ "result" : "success", "is_authenticated":session["authenticated"], "id":ord(fl.current_user.get_id()),"loginmessage":loginmessage})
 
                 else:
+                    # Unable to log in the user with flask-login so change the login message to Failed to authenticate
                     loginmessage = "Failed to authenticate"
+
+            # An error occured when trying to log in the user
             except Exception as e:
                 loginmessage = str(e)
 
