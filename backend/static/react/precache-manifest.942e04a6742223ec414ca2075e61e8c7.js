e68923c78338ce36a38377cef70e9a3afc961357
AlexB02 <alexanderdb8@gmail.com>
2019-10-22 19:43:04 +0100

    Build

---
+++ backend/static/react/precache-manifest.942e04a6742223ec414ca2075e61e8c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cd3a64a14cd2b104932e0117d7419ec",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "46fbca522f0f2f0c96b2",
+    "url": "/static/react/css/main.d04fbbbd.chunk.css"
+  },
+  {
+    "revision": "3f44179173c65a1eb0675346cafd1ecc",
+    "url": "/static/react/favicon.ico"
+  },
+  {
+    "revision": "0f08c8de6833e1db2c79",
+    "url": "/static/react/js/2.299d2e07.chunk.js"
+  },
+  {
+    "revision": "46fbca522f0f2f0c96b2",
+    "url": "/static/react/js/main.9b5e4906.chunk.js"
+  },
+  {
+    "revision": "d5017f7cc57faaa6af6c",
+    "url": "/static/react/js/runtime~main.e980ba3a.js"
+  },
+  {
+    "revision": "82939c394f04213f657b2e762817894f",
+    "url": "/static/react/media/crown.82939c39.svg"
+  }
+]);
