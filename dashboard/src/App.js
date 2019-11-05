db94d0fecda5d1333325419751fe21d5477acb42
AlexB.02 <alexanderdb8@gmail.com>
2019-11-05 01:03:45 +0000

    Dashboard v1.1 and styling

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import styled from "styled-components";
 import './App.css';
 import $ from 'jquery';
+import uparrow from "./img/up-arrow.svg";
+import downarrow from "./img/down-arrow.svg";
+import ReactCSSTransitionGroup from "react-addons-css-transition-group";
 
 const Tracker = styled.div`
   width: 100%;
@@ -16,7 +19,7 @@ const Tracker = styled.div`
 const ProgressFillUp = styled.div`
   width: ${props => props.percentage}%;
   height: 20px;
-  background: #00CC66;
+  background-color: ${props => props.colour};
   border-radius: 1px;
 `
 
@@ -24,17 +27,82 @@ class ProgressBar extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {"fillup":(props.efficiency/200)*100};
+    this.state = {"fillup":(props.efficiency/200)*100,"barColour":"#00CC66"};
+
+    if (props.efficiency >= 100) {
+      this.state.barColour = "#00CC66";
+    }
+    else if (props.efficiency >= 50) {
+      this.state.barColour = "#FF8429";
+    }
+    else if (props.efficiency < 50){
+      this.state.barColour = "#DE614A";
+    }
   }
 
   render() {
     return (
       <html>
       <Tracker>
-      <ProgressFillUp percentage={this.state.fillup}/>
+      <ProgressFillUp percentage={this.state.fillup} colour={this.state.barColour}/>
       </Tracker>
       </html>
     )
+  }
+}
+
+const ToolTipText = styled.span`
+  opacity: ${props => props.visible};
+  padding: 10px;
+  z-index: 1;
+  position: absolute;
+  background-color: #FFB5A5
+  border-radius: 5px;
+  margin-left: ${props => props.leftMargin}px;
+  margin-top: -7px;
+`;
+
+class EfficiencySkillAbbrv extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {"abbrv":props.abbrv,"skillTitle":props.skillTitle,"efficiency":props.efficiency,"ToolTipTextVisible":0}
+  }
+
+  componentWillReceiveProps (props) {
+    if (props.abbrv) {
+      this.setState({"abbrv":props.abbrv})
+    }
+    else if (props.skillTitle) {
+      this.setState({"skillTitle":props.skillTitle})
+    }
+    else if (props.efficiency) {
+      this.setState({"efficiency":props.efficiency});
+    }
+  }
+
+  updateVisibility = () => {
+    if (this.state.ToolTipTextVisible === 0) {
+      this.setState({"ToolTipTextVisible":0.95})
+    }
+    else {
+      this.setState({"ToolTipTextVisible":0})
+    }
+  }
+
+  render() {
+    if (this.state.abbrv) {
+      return (
+        <html>
+          <button onClick={this.updateVisibility} class="progressBarSkill" title="Click to view more detail">{this.props.abbrv}</button>
+          <ToolTipText visible={this.state.ToolTipTextVisible} leftMargin={Math.round(30-1.5*this.props.abbrv.length)}>{this.props.skillTitle}: {this.props.efficiency}/200</ToolTipText>
+        </html>
+      )
+    }
+    else {
+      return (
+        <div>Error</div>
+      )
+    }
   }
 }
 
@@ -42,24 +110,29 @@ class EfficiencyProgress extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {"skillAbbrv":props.skillAbbrv,"skillTitle":props.skillTitle,"efficiency":props.efficiency,"percentageEfficiency":Math.round((props.efficiency/200)*100)};
+    this.state = {"percentageEfficiency":Math.round((props.efficiency/200)*100)};
   }
 
   render() {
-    return (
-      <html>
-        <div class="efficiencyWidgetTooltip">
+    this.state = {"percentageEfficiency":Math.round((this.props.efficiency/200)*100)};
+    if (this.props) {
+      return (
+        <html>
           <div class="progressBarEfficiencyValue">{this.state.percentageEfficiency}%</div>
           <table class="EfficienciesTable" align="centre">
             <tr>
-              <td class="progressBarSkillTD"><div class="progressBarSkill">{this.state.skillAbbrv}</div></td>
-              <td class="progressBarBarTD"><div class="progressBarBar"><ProgressBar efficiency={this.state.efficiency}/></div></td>
+              <td class="progressBarSkillTD"><EfficiencySkillAbbrv class="progressBarSkill" abbrv={this.props.skillAbbrv} skillTitle={this.props.skillTitle} efficiency={this.props.efficiency}/></td>
+              <td class="progressBarBarTD"><div class="progressBarBar"><ProgressBar efficiency={this.props.efficiency}/></div></td>
             </tr>
           </table>
-          </div>
-          <span class="efficiencyWidgetTooltipText">{this.state.skillTitle}: {this.state.efficiency}/200</span>
-      </html>
-    )
+        </html>
+      )
+    }
+    else {
+      return (
+        <div>No props</div>
+      )
+    }
   }
 }
 
@@ -67,7 +140,13 @@ class EfficienciesWidget extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {"title":"","data":[],"presentableData":"","length":5};
+    this.state = {
+      "title":"",
+      "data":[],
+      "presentableData":"",
+      "length":5,
+      "lengthIfDataSmallerThanLength":props.data.length
+  };
     this.setState({"data":props.data});
     this.setState({"presentableData": this.dataToPresent(props.data)});
     this.state.title = props.title;
@@ -75,47 +154,86 @@ class EfficienciesWidget extends React.Component {
   };
 
   componentWillReceiveProps(nextProps) {
-    if (nextProps !== this.props.value) {
+    if (nextProps !== this.props) {
       if (nextProps.data) {
         this.setState({"data": nextProps.data});
         this.setState({"presentableData": this.dataToPresent(nextProps.data)});
+
       }
-      else if (nextProps.title) {
+      if (nextProps.title) {
         this.setState({"title":nextProps.title});
       }
-      else {
-        this.setState({"title":nextProps.title});
-        this.setState({"data": nextProps.data});
-        this.setState({"presentableData": this.dataToPresent(nextProps.data)});
+      if (nextProps.length) {
+        this.setState({"length":nextProps.length});
       }
     }
   }
 
-  dataToPresent(data) {
+  updateLength = () => {
+    if (this.state.length > this.state.data.length) {
+      return this.state.data.length;
+    }
+    else {
+      return this.state.length;
+    }
+  }
+
+  dataToPresent = (data) => {
     var presentable;
 
     if (!data) {
-      presentable = [];
+      return (
+        <div>No data in props</div>
+      )
     }
     else {
       try {
-        if (data.length < this.state.length) {
-          this.setState({"length":data.length});
-          presentable = data.slice(0,data.length);
+        if ((this.state.title) && (data.length) && !(this.state.length === 0)) {
+
+          if (data.length < this.state.length) {
+            this.setState({"length":data.length});
+            presentable = data.slice(0,data.length);
+          }
+
+          else {
+            presentable = data.slice(0,this.state.length);
+          }
+
+          return (
+            <span>
+            {presentable && presentable.length && presentable.map((skill, i) => React.createElement(EfficiencyProgress, {"skillTitle":skill["skillTitle"],"skillAbbrv":skill["skillAbbrv"],"efficiency":skill["avg"]}))}
+            </span>
+          );
         }
-        else {
-          presentable = data.slice(0,this.state.length);
+        else if (!this.state.title) {
+          return (
+            <div>No title</div>
+          )
+        }
+        else if (!data.length) {
+          const divStyle = {
+            margin: '10px',
+            "padding-left": "3vw",
+            "padding-right": "3vw"
+          };
+          return (
+            <html style={divStyle}>
+              <div>You don't have any skills yet!</div><br/>
+              <div>Complete tasks to see your statistics</div>
+             </html>
+          )
+        }
+        else if (this.state.length === 0) {
+          return (
+            <div>No length</div>
+          )
         }
       }
-      catch (e) {}
+      catch (e) {
+        console.log("Present data error");
+        return;
+      }
     }
-
-    return (
-
-      <div>
-      {presentable && presentable.length && presentable.map((skill, i) => React.createElement(EfficiencyProgress, {"skillTitle":skill["skillTitle"],"skillAbbrv":skill["skillAbbrv"],"efficiency":skill["avg"]}))}
-      </div>
-    );
   };
 
   increaseLength = () => {
@@ -133,30 +251,102 @@ class EfficienciesWidget extends React.Component {
 
   render() {
     return (
-      <html className="widget">
-        <span class="EfficiencyWidgetTitleBar">
-          <b>{this.state.title} </b>
-          <button onClick={this.increaseLength}>Up</button>
-          <button onClick={this.decreaseLength}>Down</button>
-        </span>
-        <div class="efficiencyWidgetData">{this.state.presentableData}</div>
-      </html>
+        <html className="widget">
+          <span class="EfficiencyWidgetTitleBar">
+            <b>{this.state.title} </b>
+            {this.updateLength()}/{this.state.data.length}
+            <button class="ArrowButton" onClick={this.increaseLength} title="Show more"><img class="arrowsvg" src={downarrow} title="Show more"/></button>
+            <button class="ArrowButton" onClick={this.decreaseLength} title="Show less"><img class="arrowsvg" src={uparrow} title="Show less"/></button>
+          </span>
+          <div class="efficiencyWidgetData">{this.state.presentableData}</div>
+        </html>
     )
   };
 };
+
+class DropdownMenu extends React.Component {
+  constructor(props) {
+    console.log("Creating new dropdownmenu");
+    super(props);
+    this.state = {"title":props.title || "title",
+                  "items":props.items};
+
+    console.log("Initial items prop: "+JSON.stringify(props.items));
+  }
+
+  componentWillReceiveProps(props) {
+    if (props.title) {
+      this.setState({"title":props.title});
+    }
+    if (props.items) {
+      console.log("Received new items prop: "+JSON.stringify(props.items));
+      this.setState({"items":props.items});
+    }
+  }
+
+  selectChange = (event) => {
+
+    console.log("Change page event");
+
+    for(var i = 0; i < this.props.items.length; i++) {
+
+      if(this.props.items[i]["title"] === event.target.value) {
+        this.props.triggerParentUpdate(event.target.value,this.props.items[i]["id"]);
+        return;
+      }
+
+    }
+    this.props.triggerParentUpdate(event.target.value,0);
+
+  }
+
+  render() {
+    console.log("this.state.title: "+this.state.title);
+    if (this.state.title === "Your Profile") {
+      console.log("Is profile");
+      return (
+        <html>
+          <select id="ProjectViewDropdown" class="ProjectViewDropdown" onChange={this.selectChange}>
+            <option value={"profile"}>{this.props.title}</option>
+            {this.state.items && this.state.items.length && this.state.items.map((project,i) => <option value={project["title"]}>{project["title"]}</option>)}
+          </select>
+        </html>
+      )
+    }
+    else {
+      console.log("Not profile");
+      var projectid = this.props.projectid;
+      console.log("Projectid: "+projectid);
+      return (
+        <html>
+          <select id="ProjectViewDropdown" class="ProjectViewDropdown" onChange={this.selectChange}>
+            <option value={this.state.title}>{this.state.title}</option>
+            {this.state.items && this.state.items.length && this.state.items.map((project,i) => <option value={project["title"]}>{project["title"]}</option>)}
+            <option value="profile">Your profile</option>
+          </select>
+        </html>
+      )
+    }
+  }
+
+}
 
 class NavBar extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {"username":"","page":""};
-    this.setState({"username":props.username});
-    this.setState({"page":props.page});
+    this.state = {username:"",page:"",projects:[]};
+    this.setState({username:props.username});
+    this.setState({page:props.page});
+    this.setState({projects:props.projects});
   }
 
   componentWillReceiveProps(props) {
-    this.setState({"username":props.username});
-    this.setState({"page":props.page});
+    this.setState({username:props.username});
+    this.setState({page:props.page});
+    if (props.projects) {
+      this.setState({projects:props.projects});
+    }
   }
 
   logOut = () => {
@@ -167,41 +357,43 @@ class NavBar extends React.Component {
     return (
       <html>
       <div className="NavBar">
-      <a class="pageStateNavBar">{this.state.page}</a>
-      <b><a>Welcome, {this.state.username}</a></b>
-      <b><a><button onClick={this.logOut}>Log Out</button></a></b>
+        <b><a><button onClick={this.logOut}>Log Out</button></a></b>
+        <b><a>Welcome, {this.state.username}</a></b>
+        <a class="pageStateNavBar">Currently Viewing: <DropdownMenu title={this.state.page} items={this.props.projects} triggerParentUpdate={this.props.triggerParentUpdate} projectid={this.props.projectid}/>
+        </a>
       </div>
       </html>
   )
   }
 }
 
-class Profile extends React.Component {
+class ProfilePage extends React.Component {
 
 ///////////////////////////////////////////////////////////////////
 // Initialisation function/////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////
 
-  getUserData = () => {
+  getUserProfileData = () => {
     let _this = this;
     $(document).ready(function(){
       // Get user details
-      var req = $.ajax({url: "/getuser",
-                        type: "POST" ,
-                        data: []
+      var req = $.ajax({url: "/getuserprofile",
+                        type:"POST",
+                        data: {}
                       });
-
       try {
-        req.done(function(data) {
-          _this.setState({username: data.username});
-          _this.setState({timeEfficienciesMax: data.timeEfficienciesMax});
-          _this.setState({timeEfficienciesMin: data.timeEfficienciesMin});
-          _this.setState({scheduleEfficienciesMax: data.scheduleEfficienciesMax});
-        });
+        req.done(function(data){
+            _this.setState({username: data.username});
+            _this.setState({projects: data.projects});
+            _this.setState({timeEfficienciesMax: data.timeEfficienciesMax});
+            _this.setState({timeEfficienciesMin: data.timeEfficienciesMin});
+            return;
+        })
       }
-      catch (e) {};
-    });
-  };
+      catch (e) {
+        console.log("Error");
+      };
+    })};
 
   constructor(props) {
 
@@ -210,34 +402,47 @@ class Profile extends React.Component {
     // Setup all state variables for the body
     this.state = {
       username: "",
-      timeEfficienciesMax: {},
-      timeEfficienciesMin: {},
-      scheduleEfficienciesMax: {}
+      projects: [],
+      timeEfficienciesMax: [],
+      timeEfficienciesMin: []
     }
-    this.getUserData();
-
+    this.getUserProfileData();
   }
 
+  addEfficiency = () => {
+    let _this = this;
+    $(document).ready(function(){
+      // add efficiency
+      var req = $.ajax({url: "/addefficiency",
+                        type:"POST",
+                        data: {}
+                      });
+      try {
+        req.done(function(data){});
+      }
+      catch (e) {};
+    })
+    window.location.reload();
+  };
+
   render() {
+    console.log(this.state.projects);
       return (
       <html>
-      <NavBar className="NavBar" username={this.state.username} page="Your Profile"/>
+      <NavBar className="NavBar" username={this.state.username} projects={this.state.projects} page={"Your Profile"} triggerParentUpdate={this.props.triggerParentUpdate} projectid={0}/>
       <body className="Body">
+      <button onClick={this.addEfficiency} />
       <div className="widgets">
         <div className="widgets-column">
-          <EfficienciesWidget title="Best Max Time Efficiencies" data={this.state.timeEfficienciesMax} length={5} />
+          <EfficienciesWidget title="Your best skills - Time Left" data={this.state.timeEfficienciesMax} length={this.state.timeEfficienciesMax.length} />
           <div className="verticalWidgetGap"/>
-          <EfficienciesWidget title="Best Max Time Efficiencies" data={this.state.timeEfficienciesMax} length={5} />
-          <div className="verticalWidgetGap"/>
-          <EfficienciesWidget title="Best Max Time Efficiencies" data={this.state.timeEfficienciesMax} length={5} />
+          <EfficienciesWidget title="Your worst skills - Time Left" data={this.state.timeEfficienciesMin} length={5} />
         </div>
         <div className="horizontalWidgetGap" />
         <div className="widgets-column">
-          <EfficienciesWidget title="Worst Min Time Efficiencies" data={this.state.timeEfficienciesMin} length={5} />
+          <EfficienciesWidget title="Your worst skills - Time Right" data={this.state.timeEfficienciesMin} length={3} />
           <div className="verticalWidgetGap"/>
-          <EfficienciesWidget title="Best Max Time Efficiencies" data={this.state.timeEfficienciesMax} length={5} />
-          <div className="verticalWidgetGap"/>
-          <EfficienciesWidget title="Best Max Time Efficiencies" data={this.state.timeEfficienciesMax} length={5} />
+          <EfficienciesWidget title="Your best skills - Time Right" data={this.state.timeEfficienciesMax} length={9} />
         </div>
       </div>
       </body>
@@ -250,10 +455,78 @@ class Profile extends React.Component {
   };
 };
 
-export class Body extends React.Component {
+class ProjectPage extends React.Component {
+
+  constructor(props) {
+    console.log("Project page");
+    super(props);
+
+    // Setup all state variables for the body
+    this.state = {
+      username: "",
+      projects: [],
+      projectid: 0,
+      title: ""
+    }
+    this.setState({projectid:props.projectid});
+    this.getUserProjectData(props.projectid);
+  }
+
+  getUserProjectData = (projectid) => {
+    let _this = this;
+    $(document).ready(function(){
+      // Get user details
+      var req = $.ajax({url: "/getuserproject",
+                        type: "POST" ,
+                        data: JSON.stringify({projectid}),
+                        dataType: "json",
+                        contentType: "application/json;charset=utf-8",
+                      });
+
+      try {
+        req.done(function(data) {
+          _this.setState({username: data.username});
+          _this.setState({projects: data.projects});
+          _this.setState({title: data.title});
+        });
+      }
+      catch (e) {};
+    });
+  };
+
   render() {
     return (
-      <Profile />
+      <NavBar className="NavBar" username={this.state.username} page={this.state.title} projects={this.state.projects} triggerParentUpdate={this.props.triggerParentUpdate} projectid={this.state.projectid}/>
     )
+  }
+}
+
+export class Body extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {page:"",projectid:0}
+    this.state.page = "profile";
+  }
+
+  updatePage = (page,projectid) => {
+      console.log("set state to: "+page);
+      this.setState({"page":page});
+      this.setState({"projectid":projectid});
+      console.log("Projectid: "+projectid);
+      console.log("Projectid state: "+this.state.projectid);
+  }
+
+  render() {
+    if (this.state.page === "profile") {
+      console.log("Profile page");
+      return (
+        <ProfilePage triggerParentUpdate={this.updatePage}/>
+      )
+    }
+    else {
+      return (
+        <ProjectPage projectid={this.state.projectid} triggerParentUpdate={this.updatePage}/>
+      )
+    }
   }
 }
