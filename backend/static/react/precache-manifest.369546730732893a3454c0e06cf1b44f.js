3e6eab27626d7a4119bf34ea37b84387f3d4694d
AlexB.02 <alexanderdb8@gmail.com>
2019-11-06 00:59:56 +0000

    Build, updated route package.json homepage

---
+++ backend/static/react/precache-manifest.369546730732893a3454c0e06cf1b44f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0d47b7b887d8704d0cdf767a3b034ca",
+    "url": "/static/react/../../templatefolder/dashboard.html"
+  },
+  {
+    "revision": "1f883ff58847caf405d6",
+    "url": "/static/react/css/main.d420afb4.chunk.css"
+  },
+  {
+    "revision": "3f44179173c65a1eb0675346cafd1ecc",
+    "url": "/static/react/favicon.ico"
+  },
+  {
+    "revision": "39ceb6997c74efa829cd",
+    "url": "/static/react/js/2.b1787991.chunk.js"
+  },
+  {
+    "revision": "1f883ff58847caf405d6",
+    "url": "/static/react/js/main.056d53c1.chunk.js"
+  },
+  {
+    "revision": "690f4a15a57ea653a76c",
+    "url": "/static/react/js/runtime~main.5e2fdba1.js"
+  },
+  {
+    "revision": "852e4fbe099fc87a78f8ff0a2d4a1721",
+    "url": "/static/react/media/down-arrow.852e4fbe.svg"
+  },
+  {
+    "revision": "305e26261d0b10c6a9dfe2f4ae694a56",
+    "url": "/static/react/media/up-arrow.305e2626.svg"
+  }
+]);
